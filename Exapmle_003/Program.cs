b1c0daae1c70dbee5183adb0d342114a46c4a1c0
«Vitaly» <vitalik.yakhimovich@mail.ru>
2023-03-20 18:00:59 +0300

    add Example 003

---
+++ Exapmle_003/Program.cs
@@ -0,0 +1,10 @@
+﻿  Console.WriteLine("Введите число:");
+ int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 1)
+            {
+                Console.WriteLine($"Число {num} Нечетное");
+            }
+            else
+            
+                Console.WriteLine($"Число {num} четное");
