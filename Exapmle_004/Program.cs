f6dbd4607917474a29d3fde72d350ae18438fd58
«Vitaly» <vitalik.yakhimovich@mail.ru>
2023-03-20 18:03:56 +0300

    add Example 004

---
+++ Exapmle_004/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int i = 1; 
+
+while( i < num)
+{
+    if ( i % 2 == 0)
+    {
+        Console.Write($"{i} , ");
+    }
+i++;
+   
+}
